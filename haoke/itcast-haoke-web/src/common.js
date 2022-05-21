a7de57acd906fe3dea8c593bc474af5048d1f8df
zhiwuyazhe-z <76238667+zhiwuyazhe-z@users.noreply.github.com>
2022-05-21 20:10:08 +0800

    Delete haoke/itcast-haoke-web directory

--- haoke/itcast-haoke-web/src/common.js
+++
@@ -1,8 +0,0 @@
-export default {
-  // imgBaseUrl: 'http://47.96.21.88:8086/',
-  // apiBaseUrl: 'http://47.96.21.88:8086/',
-  // wsBaseUrl: 'ws://47.96.21.88:8087'
-  imgBaseUrl: 'http://192.168.8.128:8086/',
-  apiBaseUrl: 'http://192.168.8.128:8086/',
-  wsBaseUrl: 'ws://192.168.8.128:18081/ws'
-}
